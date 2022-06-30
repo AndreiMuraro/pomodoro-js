c108850f793595e8acc74ba1e6fdbb9921500e18
Andrei Muraro <muraroandrei@gmail.com>
2022-06-30 18:35:14 -0300

    feat: add `pause` to resume, fix circle

--- settings.js
+++ settings.js
@@ -1,28 +1,37 @@
-const focusTimeInput = document.querySelector('#focusTime');
-const breakTimeInput = document.querySelector('#breakTime');
+const focusTimeInput = document.querySelector("#focusTime");
+const breakTimeInput = document.querySelector("#breakTime");
+const pauseBtn = document.querySelector(".btn-pause");
 
-document.querySelector("form").addEventListener('submit', (e) => {
+focusTimeInput.value = localStorage.getItem("focusTime");
+breakTimeInput.value = localStorage.getItem("breakTime");
+
+document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     localStorage.setItem("focusTime", focusTimeInput.value);
     localStorage.setItem("breakTime", breakTimeInput.value);
 });
 
-document.querySelector(".btn-reset").addEventListener('click', () => {
-    startBtn.innerHTML = "start";
+document.querySelector(".btn-reset").addEventListener("click", () => {
+    startBtn.style.transform = "scale(1)";
     clearTimeout(initial);
     setProgress(0);
-    minutes.textContent = "00";
+    minutes.textContent = "0";
     seconds.textContent = "00";
 });
 
-pauseBtn.addEventListener('click', () => {
-    if (paused === undefined){
-        return;
-    } if (paused){
-        pauseBtn.innerHTML = "pause";
-    } else {
-        clearTimeout(initial);
-        pause = true;
-    }
-
+pauseBtn.addEventListener("click", () => {
+  if (paused === undefined) {
+    return;
+  }
+  if (paused) {
+    paused = false;
+    initial = setTimeout("decremenT()", 60);
+    pauseBtn.textContent = "pause";
+    pauseBtn.classList.remove("resume");
+  } else {
+    clearTimeout(initial);
+    pauseBtn.textContent = "resume";
+    pauseBtn.classList.add("resume");
+    paused = true;
+  }
 });
--- timer.js
+++ timer.js
@@ -1,51 +1,63 @@
 const el = document.querySelector(".clock");
-const bell = document.querySelector(".audio");
+const bell = document.querySelector("audio");
 
 const minutes = document.querySelector(".mins");
 const seconds = document.querySelector(".secs");
 
 const startBtn = document.querySelector(".btn-start");
-
 localStorage.setItem("btn", "focus");
 
 let initial, totalsecs, perc, paused, mins, secs;
 
 // INICIA O TIMER E OBTÉM O TEMPO INICIAL
-startBtn.addEventListener("click", () =>{
-    let btn = localStorage.getItem("btn");
-    if(btn === "focus"){
-        mins = +localStorage.getItem("focusTime");
-        startBtn.innerHTML = "pause";
-    } else {
-        mins = +localStorage.getItem("breakTime");
-        startBtn.innerHTML = "start";
-    }
+startBtn.addEventListener("click", () => {
+  let btn = localStorage.getItem("btn");
 
-    secs = mins * 60;
-    totalsecs = seconds * 60;
-    setTimeout(decremenT(), 60);
-})
+  if (btn === "focus") {
+    mins = +localStorage.getItem("focusTime") || 1;
+  } else {
+    mins = +localStorage.getItem("breakTime") || 1;
+  }
 
+  secs = mins * 60;
+  totalsecs = mins * 60;
+  setTimeout(decremenT(), 60);
+  startBtn.style.transform = "scale(0)";
+  paused = false;
+});
 // CONTAGEM REGRESSIVA
-function decremenT(){
-    minutes.textContent = Math.floor(secs / 60);
-    seconds.textContent = secs % 60 > 9 ? secs % 60 : `0${secs % 60}`;
+function decremenT() {
+  minutes.textContent = Math.floor(secs / 60);
+  seconds.textContent = secs % 60 > 9 ? secs % 60 : `0${secs % 60}`;
+  if (circle.classList.contains("end-time")) {
+    circle.classList.remove("end-time");
+  }
 
-    if (secs > 0){
-        secs--;
-        initial = setTimeout(decremenT, 1000);
-    } else {
-        bell.play();
-        mins = 0;
-        secs = 0;
-        bell.play();
-        let btn = localStorage.getItem("btn");
-
-        if (btn === "focus"){
-            startBtn.textContent = "start break";
-            startBtn.classList.add("break");
-            localStorage.setItem("btn", "break");
-            localStorage.setItem("btn", "focus");
-        }
+  if (secs > 0) {
+    perc = Math.ceil(((totalsecs - secs) / totalsecs) * 100);
+    setProgress(perc);
+    secs--;
+    initial = window.setTimeout("decremenT()", 1000);
+    if (secs < 10) {
+      circle.classList.add("end-time");
     }
+  } else {
+    mins = 0;
+    secs = 0;
+    bell.play();
+    let btn = localStorage.getItem("btn");
+
+    if (btn === "focus") {
+      startBtn.textContent = "iniciar pausa";
+      startBtn.classList.add("break");
+      localStorage.setItem("btn", "break");
+    } else {
+      startBtn.classList.remove("break");
+      startBtn.textContent = "iniciar";
+      localStorage.setItem("btn", "focus");
+    }
+    startBtn.style.transform = "scale(1)";
+  }
 }
+
+
